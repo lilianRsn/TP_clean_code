2d705b3f401814038b432d7e54c16fd91cec7c2a
Lilian Reisinho <lilianreisinho@gmail.com>
2025-10-30 14:30:38 +0100

    premier test initialisé + fonction instanciée

---
+++ index.js
@@ -0,0 +1,4 @@
+//le paramètre de la fonction doit etre un tableau de plusieurs lancers de dés
+
+export const analyserLancer = (lancer) => {
+};
--- index.test.js
+++ index.test.js
@@ -1,1 +1,12 @@
 import { describe, expect, it } from "vitest";
+
+describe("Doit retourner le nom de la figure détectée", () => {
+    it("Doit retourner la figure détectée", () => {
+        const lancers = [
+            [2, 2, 3, 3, 3],  // Full
+            [6, 6, 6, 6, 2],  // Carré
+            [1, 2, 3, 4, 5],  // Grande suite
+        ];
+        expect(analyserLancer(lancers)).toBe(0);
+    });
+});
